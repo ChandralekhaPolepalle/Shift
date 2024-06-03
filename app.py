a2ddf1db91f5526b2e98902db12b855524badc71
Chandralekha <pchandralekha99@gmail.com>
2024-06-03 01:11:35 -0400

    feat: Created products overview page

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ def index():
 def subscription_form():
     return render_template('subscription_form.html')
 
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
